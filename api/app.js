6f41efe87b52f6cc57924003cef00f6b03e918d2
David Stanimirovic <=>
2023-03-26 13:28:23 +0200

    Created initial project structure

---
+++ api/app.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(() => {
+  console.log(`Server running at ${process.env.BASE_URL}/`);
+});
